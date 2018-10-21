00df6533d2cc441832a71b3a72d8a57c013ae89e
chanakya <chanakya.valluri@gmail.com>
2018-10-21 00:35:48 -0700

    clean up files

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,19 +1,8 @@
 console.log("Starting app.js");
 
 const fs = require("fs");
-const os = require("os");
 const _ = require("lodash");
+
 const notes = require("./notes.js");
 
-var filteredArray = _.uniq([1,1,2,4,4,"Nick", "Nick"]);
-console.log(filteredArray);
-
-// console.log(_.isString(true));
-// console.log(_.isString("Nick"));
-
-// var result = notes.addNote();
-// console.log(result);
-
-// var user = os.userInfo();
-//
-// fs.appendFileSync("greeting.txt", `Hello ${user.username}!`);
+console.log(process.argv);
