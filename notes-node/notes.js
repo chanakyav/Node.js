aedbf0d16512d3ccdcfcfb7d1024b532732c9a13
chanakya <chanakya.valluri@gmail.com>
2018-10-21 13:26:38 -0700

    Reading notes and added reusable functions

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -31,11 +31,13 @@ var addNote = (title, body) => {
 
 var getAll = () => {
   console.log("Getting all notes");
-}
+};
 
 var getNote = (title) => {
-  console.log("Getting note", title);
-}
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title === title);
+  return filteredNotes[0];
+};
 
 var removeNote = (title) => {
   var notes = fetchNotes();
@@ -43,11 +45,18 @@ var removeNote = (title) => {
   saveNotes(filteredNotes);
 
   return notes.length !== filteredNotes.length;
+};
+
+var logNote = (note) => {
+  console.log("--");
+  console.log(`Title: ${note.title}`);
+  console.log(`Body: ${note.body}`);
 }
 
 module.exports = {
   addNote: addNote,
   getAll,          // Identical to above
   getNote,
-  removeNote
+  removeNote,
+  logNote
 };
