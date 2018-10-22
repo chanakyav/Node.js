e12cc51d979961d6c7e4806ddbe570ef2e3ca99d
chanakya <chanakya.valluri@gmail.com>
2018-10-21 22:03:11 -0700

    started weather app. testing requests to google map api

---
+++ weather-app/app.js
@@ -0,0 +1,9 @@
+const request = require('request');
+
+request({
+  url: "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDY2MZYRXikZv-x6LjjBInYqcRuwcLHB8A&address=1301%20lombard%20street%20philadelphia",
+  json: true
+}, (error, response, body) => {
+  //console.log(JSON.stringify(body, undefined, 2)); // 2 - spaces for indentation
+  console.log(`Location: ${JSON.stringify(body.results[0].geometry.location)}`);
+});
