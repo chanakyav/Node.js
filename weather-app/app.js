cf5ad8bcb4aa7225e3141a0821a7ed2aaaa35cf2
chanakya <chanakya.valluri@gmail.com>
2018-10-21 22:28:03 -0700

    trying to fix github vulnerabilities

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,5 +5,6 @@ request({
   json: true
 }, (error, response, body) => {
   //console.log(JSON.stringify(body, undefined, 2)); // 2 - spaces for indentation
-  console.log(`Location: ${JSON.stringify(body.results[0].geometry.location)}`);
+  console.log(`Latitude: ${JSON.stringify(body.results[0].geometry.location.lat)}`);
+  console.log(`Latitude: ${JSON.stringify(body.results[0].geometry.location.lng)}`);
 });
